79627e72643ff2b24b57bf78fb19ddea9c228163
Nathan Edmund Fanlau <nathanfanlau@gmail.com>
2024-09-24 17:37:12 +0700

    Design UI of landing page/about me page

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 new fullpage('#fullPage', {
-  autoScrolling: true
+  autoScrolling: true,
+  navigation: true,
+  navigationTooltips: ['ABOUT ME', 'CALM', 'WATCHGOODS', 'PROJECT 3'],
+  showActiveTooltip: true,
+  navigationPosition: 'left',
+  continuousVertical: true,
 })
